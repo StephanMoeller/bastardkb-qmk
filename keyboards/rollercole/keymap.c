209298f760fc1db7bf0e366b41fcf3772b21b29d
StephanMoeller <stephanryer@hotmail.com>
2024-10-21 23:45:10 +0200

    added right click to layout

--- keyboards/rollercole/keymap.c
+++ keyboards/rollercole/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        LT(BOOT, KC_Q),    KC_W,   _W(KC_R),      KC_P,     KC_B,                KC_K,         KC_L,   _W(KC_O), KC_U,LT(BOOT,DK_SINGLE_QT),
         KC_F,        _A(KC_A),  _C(KC_S),  _S(KC_T),       KC_G,                KC_M,     _S(KC_N),   _C(KC_E),   _A(KC_I),             KC_Y,
         KC_Z,            KC_X,      KC_C,LT(TMUX,KC_D),    KC_V,          __________,LT(TMUX,KC_H),    DK_COMM,  LT(APP_LAUNCH, DK_DOT),  DK_MINS,
-                              MO(F_KEYS),THUMB_LEFT,   MO(TMUX),          MO(TMUX),THUMB_RIGHT,  MO(F_KEYS)
+     LT(F_KEYS,QK_MOUSE_BUTTON_2),THUMB_LEFT, QK_MOUSE_BUTTON_2,          MO(TMUX),THUMB_RIGHT,  MO(F_KEYS)
     ),
  [ARROW] = ROLLERCOLE_36(
         DK_EXCLAM,    DK_LABK,  DK_EQUALS,    DK_RABK,    KC_PERC,              DK_SLASH,    KC_HOME,      KC_UP,      KC_END, DK_QSTMRK,
